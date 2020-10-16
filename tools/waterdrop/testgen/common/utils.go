6101f05ae233699277d03f905ef6cc017f578bcd
John Sun <sunforcherry@gmail.com>
2020-10-16 15:23:44 +0800

    polish code

--- tools/waterdrop/testgen/common/utils.go
+++ tools/waterdrop/testgen/common/utils.go
@@ -25,16 +25,16 @@ import (
 	"golang.org/x/tools/imports"
 )
 
-const GenTestSuccess = `                                            
-							江城子·程序记梦
-						十年编程两茫茫，工期短，需求长。
-							百行代码，Bug到处藏。
-						纵使上线又如何，新版本，继续忙。
-						黑白颠倒没商量，趴桌上，进梦乡。
-							夜半梦醒，无人在身旁。
-						最怕灯火阑珊时，手机响，心里慌。
-					// Generation success. Powered by Waterdrop
-				`
+const GenTestSuccess = `
+	      江城子·程序记梦
+	十年编程两茫茫，工期短，需求长。
+	   百行代码，Bug到处藏。
+	纵使上线又如何，新版本，继续忙。
+	黑白颠倒没商量，趴桌上，进梦乡。
+	   夜半梦醒，无人在身旁。
+	最怕灯火阑珊时，手机响，心里慌。
+// Generation success. Powered by Waterdrop
+`
 
 // GoImport Use golang.org/x/tools/imports auto import pkg
 func GoImport(file string, bytes []byte) (res []byte, err error) {
