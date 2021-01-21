c8102abc616889bed5f46177c89cbaa46d9d8f86
John Sun <sunforcherry@gmail.com>
2021-01-21 14:09:34 +0800

    add mongo adapter

---
+++ pkg/database/mongo/collection.go
@@ -0,0 +1,321 @@
+/*
+ *
+ * Copyright 2021 waterdrop authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package mongo
+
+import (
+	"context"
+	"time"
+
+	"github.com/opentracing/opentracing-go/log"
+
+	"github.com/qiniu/qmgo"
+
+	"github.com/UnderTreeTech/waterdrop/pkg/trace"
+	"github.com/opentracing/opentracing-go/ext"
+)
+
+type Collection struct {
+	conn   *qmgo.Collection
+	config *Config
+	name   string
+}
+
+// Aggregate executes an aggregate command against the collection and returns a Aggregate to get resulting documents.
+func (c *Collection) Aggregate(ctx context.Context, pipeline interface{}) *Aggregate {
+	span, _ := trace.StartSpanFromContext(ctx, "aggregate")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+
+	return &Aggregate{
+		ai:     c.conn.Aggregate(ctx, pipeline),
+		ctx:    ctx,
+		config: c.config,
+		span:   span,
+	}
+}
+
+// Bulk returns a new context for preparing bulk execution of operations.
+func (c *Collection) Bulk(ctx context.Context) *Bulk {
+	bulk := c.conn.Bulk()
+	span, ctx := trace.StartSpanFromContext(ctx, "bulk")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+
+	return &Bulk{
+		bulk:   bulk,
+		config: c.config,
+		span:   span,
+		ctx:    ctx,
+	}
+}
+
+// Find find by condition filter，return Query
+func (c *Collection) Find(ctx context.Context, filter interface{}) *Query {
+	span, ctx := trace.StartSpanFromContext(ctx, "query")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+
+	query := c.conn.Find(ctx, filter)
+
+	return &Query{
+		qi:     query,
+		config: c.config,
+		span:   span,
+	}
+}
+
+// InsertOne insert one document into the collection
+// Reference: https://docs.mongodb.com/manual/reference/command/insert/
+func (c *Collection) Insert(ctx context.Context, doc interface{}) (result *qmgo.InsertOneResult, err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "insert")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	result, err = c.conn.InsertOne(ctx, doc)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+
+	return
+}
+
+// BatchInsert executes an insert command to insert multiple documents into the collection.
+// Reference: https://docs.mongodb.com/manual/reference/command/insert/
+func (c *Collection) BatchInsert(ctx context.Context, docs interface{}) (result *qmgo.InsertManyResult, err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "batch_insert")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	result, err = c.conn.InsertMany(ctx, docs)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// Upsert updates one documents if filter match, inserts one document if filter is not match, Error when the filter is invalid
+// The replacement parameter must be a document that will be used to replace the selected document. It cannot be nil
+// and cannot contain any update operators
+// Reference: https://docs.mongodb.com/manual/reference/operator/update/
+// If replacement has "_id" field and the document is exist, please initial it with existing id(even with Qmgo default field feature).
+// Otherwise "the (immutable) field '_id' altered" error happens.
+func (c *Collection) Upsert(ctx context.Context, filter interface{}, replacement interface{}) (result *qmgo.UpdateResult, err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "upsert")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	result, err = c.conn.Upsert(ctx, filter, replacement)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// UpsertId updates one documents if id match, inserts one document if id is not match and the id will inject into the document
+// The replacement parameter must be a document that will be used to replace the selected document. It cannot be nil
+// and cannot contain any update operators
+// Reference: https://docs.mongodb.com/manual/reference/operator/update/
+func (c *Collection) UpsertId(ctx context.Context, id interface{}, replacement interface{}) (result *qmgo.UpdateResult, err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "upsert_id")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	result, err = c.conn.UpsertId(ctx, id, replacement)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// UpdateOne executes an update command to update at most one document in the collection.
+// Reference: https://docs.mongodb.com/manual/reference/operator/update/
+func (c *Collection) UpdateOne(ctx context.Context, filter interface{}, update interface{}) (err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "update_one")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	err = c.conn.UpdateOne(ctx, filter, update)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// UpdateId executes an update command to update at most one document in the collection.
+// Reference: https://docs.mongodb.com/manual/reference/operator/update/
+func (c *Collection) UpdateId(ctx context.Context, id interface{}, update interface{}) (err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "update_id")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	err = c.conn.UpdateId(ctx, id, update)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// UpdateAll executes an update command to update documents in the collection.
+// The matchedCount is 0 in UpdateResult if no document updated
+// Reference: https://docs.mongodb.com/manual/reference/operator/update/
+func (c *Collection) UpdateAll(ctx context.Context, filter interface{}, update interface{}) (result *qmgo.UpdateResult, err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "update_all")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	result, err = c.conn.UpdateAll(ctx, filter, update)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// ReplaceOne executes an update command to update at most one document in the collection.
+// If UpdateHook in opts is set, hook works on it, otherwise hook try the doc as hook
+// Expect type of the doc is the define of user's document
+func (c *Collection) ReplaceOne(ctx context.Context, filter interface{}, doc interface{}) (err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "replace_one")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	err = c.conn.ReplaceOne(ctx, filter, doc)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// Remove executes a delete command to delete at most one document from the collection.
+// if filter is bson.M{}，DeleteOne will delete one document in collection
+// Reference: https://docs.mongodb.com/manual/reference/command/delete/
+func (c *Collection) Remove(ctx context.Context, filter interface{}) (err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "remove")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	err = c.conn.Remove(ctx, filter)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// RemoveId executes a delete command to delete at most one document from the collection.
+func (c *Collection) RemoveId(ctx context.Context, id interface{}) (err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "remove_id")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	err = c.conn.RemoveId(ctx, id)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// RemoveAll executes a delete command to delete documents from the collection.
+// If filter is bson.M{}，all ducuments in Collection will be deleted
+// Reference: https://docs.mongodb.com/manual/reference/command/delete/
+func (c *Collection) RemoveAll(ctx context.Context, filter interface{}) (result *qmgo.DeleteResult, err error) {
+	now := time.Now()
+	span, ctx := trace.StartSpanFromContext(ctx, "remove_all")
+	ext.PeerAddress.Set(span, c.config.Addr)
+	ext.DBType.Set(span, "mongo")
+	ext.SpanKind.Set(span, ext.SpanKindRPCClientEnum)
+	ext.DBInstance.Set(span, c.config.DBName)
+	span = span.SetTag("db.collection", c.name)
+	defer span.Finish()
+
+	result, err = c.conn.RemoveAll(ctx, filter)
+	if ok, elpase := slowLog(now, c.config.SlowQueryDuration); ok {
+		ext.Error.Set(span, true)
+		span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
---
+++ pkg/database/mongo/query.go
@@ -0,0 +1,151 @@
+/*
+ *
+ * Copyright 2021 waterdrop authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package mongo
+
+import (
+	"time"
+
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/opentracing/opentracing-go/log"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/qiniu/qmgo"
+)
+
+// Query struct definition
+type Query struct {
+	qi     qmgo.QueryI
+	config *Config
+	span   opentracing.Span
+}
+
+// Sort is Used to set the sorting rules for the returned results
+// Format: "age" or "+age" means to sort the age field in ascending order, "-age" means in descending order
+// When multiple sort fields are passed in at the same time, they are arranged in the order in which the fields are passed in.
+// For example, {"age", "-name"}, first sort by age in ascending order, then sort by name in descending order
+func (q *Query) Sort(fields ...string) *Query {
+	q.qi = q.qi.Sort(fields...)
+	return q
+}
+
+// Select is used to determine which fields are displayed or not displayed in the returned results
+// Format: bson.M{"age": 1} means that only the age field is displayed
+// bson.M{"age": 0} means to display other fields except age
+// When _id is not displayed and is set to 0, it will be returned to display
+func (q *Query) Select(projection interface{}) *Query {
+	q.qi = q.qi.Select(projection)
+	return q
+}
+
+// Skip skip n records
+func (q *Query) Skip(n int64) *Query {
+	q.qi = q.qi.Skip(n)
+	return q
+}
+
+// Hint sets the value for the Hint field.
+// This should either be the index name as a string or the index specification
+// as a document. The default value is nil, which means that no hint will be sent.
+func (q *Query) Hint(hint interface{}) *Query {
+	q.qi = q.qi.Hint(hint)
+	return q
+}
+
+// Limit limits the maximum number of documents found to n
+// The default value is 0, and 0  means no limit, and all matching results are returned
+// When the limit value is less than 0, the negative limit is similar to the positive limit, but the cursor is closed after returning a single batch result.
+// Reference https://docs.mongodb.com/manual/reference/method/cursor.limit/index.html
+func (q *Query) Limit(n int64) *Query {
+	q.qi = q.qi.Limit(n)
+	return q
+}
+
+// One query a record that meets the filter conditions
+// If the search fails, an error will be returned
+func (q *Query) One(result interface{}) (err error) {
+	now := time.Now()
+	q.span = q.span.SetOperationName("query_one")
+	defer q.span.Finish()
+
+	err = q.qi.One(result)
+	if ok, elpase := slowLog(now, q.config.SlowQueryDuration); ok {
+		ext.Error.Set(q.span, true)
+		q.span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// All query multiple records that meet the filter conditions
+// The static type of result must be a slice pointer
+func (q *Query) All(result interface{}) (err error) {
+	now := time.Now()
+	q.span = q.span.SetOperationName("query_all")
+	defer q.span.Finish()
+
+	err = q.qi.All(result)
+	if ok, elpase := slowLog(now, q.config.SlowQueryDuration); ok {
+		ext.Error.Set(q.span, true)
+		q.span.LogFields(log.String("event", "slow_query"), log.Int64("elapse", int64(elpase)))
+	}
+	return
+}
+
+// Count count the number of eligible entries
+func (q *Query) Count() (n int64, err error) {
+	return q.qi.Count()
+}
+
+// Distinct gets the unique value of the specified field in the collection and return it in the form of slice
+// result should be passed a pointer to slice
+// The function will verify whether the static type of the elements in the result slice is consistent with the data type obtained in mongodb
+// reference https://docs.mongodb.com/manual/reference/command/distinct/
+func (q *Query) Distinct(key string, result interface{}) error {
+	return q.qi.Distinct(key, result)
+}
+
+// Cursor gets a Cursor object, which can be used to traverse the query result set
+// After obtaining the CursorI object, you should actively call the Close interface to close the cursor
+// Strongly suggest use One or All
+func (q *Query) Cursor() qmgo.CursorI {
+	return q.qi.Cursor()
+}
+
+// Apply runs the findAndModify command, which allows updating, replacing
+// or removing a document matching a query and atomically returning either the old
+// version (the default) or the new version of the document (when ReturnNew is true)
+//
+// The Sort and Select query methods affect the result of Apply. In case
+// multiple documents match the query, Sort enables selecting which document to
+// act upon by ordering it first. Select enables retrieving only a selection
+// of fields of the new or old document.
+//
+// When Change.Replace is true, it means replace at most one document in the collection
+// and the update parameter must be a document and cannot contain any update operators;
+// if no objects are found and Change.Upsert is false, it will returns ErrNoDocuments.
+// When Change.Remove is true, it means delete at most one document in the collection
+// and returns the document as it appeared before deletion; if no objects are found,
+// it will returns ErrNoDocuments.
+// When both Change.Replace and Change.Remove are false，it means update at most one document
+// in the collection and the update parameter must be a document containing update operators;
+// if no objects are found and Change.Upsert is false, it will returns ErrNoDocuments.
+//
+// reference: https://docs.mongodb.com/manual/reference/command/findAndModify/
+func (q *Query) Apply(change qmgo.Change, result interface{}) error {
+	return q.qi.Apply(change, result)
+}
