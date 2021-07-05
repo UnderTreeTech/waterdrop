8d55a9331a1116791cb43f0437b71f518ebf1f5b
John Sun <sunforcherry@gmail.com>
2021-07-05 14:37:33 +0800

    [feat] 增加跨域cors中间件

---
+++ pkg/server/http/middlewares/cors.go
@@ -0,0 +1,36 @@
+/*
+ *
+ * Copyright 2021 waterdrop authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package middlewares
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type CORSConfig = cors.Config
+
+// default cors handler
+func DefaultCORS() gin.HandlerFunc {
+	return cors.Default()
+}
+
+// customer cors handler by config
+func NewCORS(config CORSConfig) gin.HandlerFunc {
+	return cors.New(config)
+}
