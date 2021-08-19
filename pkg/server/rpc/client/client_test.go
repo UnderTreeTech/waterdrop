4fee21779926da298735acda022de396cc129e29
John Sun <sunforcherry@gmail.com>
2021-08-19 15:22:29 +0800

    add rpc client test case

--- pkg/server/rpc/client/client_test.go
+++ pkg/server/rpc/client/client_test.go
@@ -17,3 +17,86 @@
  */
 
 package client
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/UnderTreeTech/waterdrop/examples/proto/demo"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/UnderTreeTech/waterdrop/pkg/log"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/UnderTreeTech/waterdrop/pkg/server/rpc/config"
+	"github.com/UnderTreeTech/waterdrop/pkg/server/rpc/server"
+)
+
+// TestClient test grpc client
+func TestClient(t *testing.T) {
+	defer log.New(nil).Sync()
+	srv := server.New(&config.ServerConfig{
+		Addr: "0.0.0.0:21819",
+	})
+	demo.RegisterDemoServer(srv.Server(), &service{})
+	srv.Start()
+	time.Sleep(time.Millisecond * 100)
+	defer srv.Stop(context.Background())
+
+	cfg := &config.ClientConfig{
+		DialTimeout: 150 * time.Millisecond,
+		Block:       false,
+		Balancer:    "round_robin",
+		Target:      "0.0.0.0:21819",
+	}
+	client := New(cfg)
+	rpc := demo.NewDemoClient(client.GetConn())
+	reply, err := rpc.SayHelloURL(context.Background(), &demo.HelloReq{Name: "waterdrop"})
+	assert.Equal(t, reply.Content, "Hello waterdrop")
+	assert.Nil(t, err)
+}
+
+// TestDialTimeout test dial timeout
+func TestDialTimeout(t *testing.T) {
+	defer log.New(nil).Sync()
+	flag := false
+	defer func() {
+		if r := recover(); r != nil {
+			flag = true
+		}
+		assert.Equal(t, flag, true)
+	}()
+
+	srv := server.New(&config.ServerConfig{
+		Addr: "0.0.0.0:21819",
+	})
+	demo.RegisterDemoServer(srv.Server(), &service{})
+	go func() {
+		time.Sleep(time.Millisecond * 100)
+		srv.Start()
+	}()
+	defer srv.Stop(context.Background())
+
+	cfg := &config.ClientConfig{
+		DialTimeout: 50 * time.Millisecond,
+		Block:       true,
+		Balancer:    "round_robin",
+		Target:      "0.0.0.0:21819",
+	}
+	client := New(cfg)
+	demo.NewDemoClient(client.GetConn())
+}
+
+type service struct{}
+
+func (s *service) SayHello(ctx context.Context, req *demo.HelloReq) (reply *emptypb.Empty, err error) {
+	reply = &emptypb.Empty{}
+	return reply, nil
+}
+
+func (s *service) SayHelloURL(ctx context.Context, req *demo.HelloReq) (reply *demo.HelloResp, err error) {
+	reply = &demo.HelloResp{Content: "Hello " + req.Name}
+	return
+}
