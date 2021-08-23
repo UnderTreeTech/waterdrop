176d2d864b95205ef53d8aa277c19e5968a6f592
John Sun <sunforcherry@gmail.com>
2021-08-23 17:53:49 +0800

    add mime types

--- pkg/store/minio/minio.go
+++ pkg/store/minio/minio.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"io"
 	"net/url"
+	"strings"
 	"time"
+
+	"github.com/UnderTreeTech/waterdrop/pkg/store"
 
 	"github.com/minio/minio-go/v7/pkg/credentials"
 
@@ -52,29 +55,57 @@ func New(cfg *Config) (clnt *MinioClient, err error) {
 	}
 
 	if cfg.ExpireTime <= 0 {
-		cfg.ExpireTime = time.Second * 5 * 60
+		cfg.ExpireTime = store.DefaultExpireTime
 	}
 
 	clnt = &MinioClient{
 		client: client,
 		config: cfg,
 	}
+
 	return
 }
 
-func (mc *MinioClient) PutObject(bucketName string, objectName string, reader io.Reader, objectSize int64) {
-	mc.client.PutObject(context.Background(), bucketName, objectName, reader, objectSize, minio.PutObjectOptions{})
+// PutObject creates an object in a bucket.
+//
+// You must have WRITE permissions on a bucket to create an object.
+//
+//  - For size smaller than 128MiB PutObject automatically does a
+//    single atomic Put operation.
+//  - For size larger than 128MiB PutObject automatically does a
+//    multipart Put operation.
+//  - For size input as -1 PutObject does a multipart Put operation
+//    until input stream reaches EOF. Maximum object size that can
+//    be uploaded through this operation will be 5TiB.
+func (mc *MinioClient) PutObject(ctx context.Context, bucketName, objectName string, reader io.Reader, objectSize int64, ext ...string) (fileUrl string, err error) {
+	po := minio.PutObjectOptions{}
+	if len(ext) > 0 {
+		po.ContentType = store.TypeByExtension(ext[0])
+	}
+
+	if _, err = mc.client.PutObject(ctx, bucketName, objectName, reader, objectSize, minio.PutObjectOptions{}); err != nil {
+		return
+	}
+
+	return mc.GetFileUrl(ctx, bucketName, objectName)
 }
 
-func (mc *MinioClient) UploadFile(bucketName string, objectName string, file string) (path string, err error) {
-	return
-}
-func (mc *MinioClient) DownloadFile(bucketName string, objectName string) (file []byte, err error) {
-	return
+// FPutObject create an object in a bucket, with contents from file at filePath
+func (mc *MinioClient) FPutObject(ctx context.Context, bucketName, objectName, path string, ext ...string) (fileUrl string, err error) {
+	po := minio.PutObjectOptions{}
+	if len(ext) > 0 {
+		po.ContentType = store.TypeByExtension(ext[0])
+	}
+
+	if _, err = mc.client.FPutObject(ctx, bucketName, objectName, path, po); err != nil {
+		return
+	}
+
+	return mc.GetFileUrl(ctx, bucketName, objectName)
 }
 
 // GetFileUrl generate presigned get object url
-func (mc *MinioClient) GetFileUrl(bucketName string, objectName string, expireTime ...int64) (path string, err error) {
+func (mc *MinioClient) GetFileUrl(ctx context.Context, bucketName string, objectName string, expireTime ...int64) (fileUrl string, err error) {
 	expired := mc.config.ExpireTime
 	if len(expireTime) > 0 {
 		expired = time.Duration(expireTime[0]) * time.Second
@@ -82,11 +113,11 @@ func (mc *MinioClient) GetFileUrl(bucketName string, objectName string, expireTi
 	// Set request parameters
 	reqParams := make(url.Values)
 	reqParams.Set("response-content-disposition", "attachment; filename="+objectName)
-	presignedURL, err := mc.client.PresignedGetObject(context.Background(), bucketName, objectName, expired, reqParams)
+	presignedURL, err := mc.client.PresignedGetObject(ctx, bucketName, objectName, expired, reqParams)
+	fileUrl = presignedURL.String()
 	if mc.config.InternalEndpoint != mc.config.ExternalEndpoint {
-		presignedURL.Host = mc.config.ExternalEndpoint
+		fileUrl = strings.Replace(fileUrl, mc.config.InternalEndpoint, mc.config.ExternalEndpoint, 1)
 	}
 
-	path = presignedURL.String()
 	return
 }
