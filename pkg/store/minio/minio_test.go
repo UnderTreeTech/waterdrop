530a8617fb5c833597c6f0593ba9e029dd0243c3
John Sun <sunforcherry@gmail.com>
2021-08-23 19:17:05 +0800

    add minio test case

--- pkg/store/minio/minio_test.go
+++ pkg/store/minio/minio_test.go
@@ -1,7 +1,8 @@
 package minio
 
 import (
-	"fmt"
+	"context"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,14 +11,34 @@ import (
 func TestGetFileUrl(t *testing.T) {
 	cfg := &Config{
 		InternalEndpoint: "localhost:9000",
-		ExternalEndpoint: "localhost:8000",
+		ExternalEndpoint: "localhost:9000",
 		Region:           "",
-		AccessKey:        "H1ZRYMN0LG5T7TP3AYQX",
-		SecretKey:        "So6itWRcVJVI8ySXfBSOf+p72+dQeDbxk9otkyR8",
+		AccessKey:        "XMFMOKB2FJWA0I9JIR62",
+		SecretKey:        "aMRKmxoRcb+Ezr5CmOmFAqFwYWPrEFA7UdtWWWOl",
 	}
 	clnt, err := New(cfg)
 	assert.Nil(t, err)
-	path, err := clnt.GetFileUrl("john", "##blog-master+#.tar.gz")
+	ctx := context.Background()
+
+	exsit, err := clnt.ExistBucket(ctx, "test")
 	assert.Nil(t, err)
-	fmt.Println(path)
+	if !exsit {
+		err = clnt.CreateBucket(ctx, "test")
+		assert.Nil(t, err)
+	}
+
+	_, err = clnt.FPutObject(ctx, "test", "minio.go", "./minio.go")
+	assert.Nil(t, err)
+
+	object, err := os.Open("./minio_test.go")
+	assert.Nil(t, err)
+	defer object.Close()
+
+	stat, err := object.Stat()
+	assert.Nil(t, err)
+	_, err = clnt.PutObject(ctx, "test", "minio_test.go", object, stat.Size(), ".jpeg")
+	assert.Nil(t, err)
+	path, err := clnt.GetFileUrl(ctx, "test", "minio_test.go")
+	assert.Nil(t, err)
+	assert.Contains(t, path, "minio_test.go")
 }
