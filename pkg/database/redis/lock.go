c5ea143185ae1fd99ad7c19e97947e887bc9979d
John Sun <sunforcherry@gmail.com>
2021-08-25 14:37:09 +0800

    polish redis lock code

--- pkg/database/redis/lock.go
+++ pkg/database/redis/lock.go
@@ -23,10 +23,7 @@ import (
 	"errors"
 	"time"
 
-	"github.com/gomodule/redigo/redis"
-
 	"github.com/UnderTreeTech/waterdrop/pkg/log"
-
 	"github.com/UnderTreeTech/waterdrop/pkg/utils/xstring"
 )
 
@@ -35,18 +32,19 @@ var (
 )
 
 const (
-	_lockTime   = 1000                  //px milliseconds
+	_lockTime   = 1000                  // px milliseconds
 	_retry      = 2                     // retry times
 	_retryDelay = 15 * time.Millisecond // retry interval, milliseconds
 )
 
-//ttl ms
-//retry 重试次数
-//retryDelay us
-//当前lock超时ttl为1s，重试2次，每15ms try-lock一次
-//请勿必只对关键业务操作路径进行加锁，缩小加锁操作范围
-//例如库存的select-for-update，保证锁操作能在15ms内完成
-func (r *Redis) lock(ctx context.Context, key string, ttl int, retry int, retryDelay time.Duration) (err error, locked bool, lockValue string) {
+// lock lock resource
+// ttl ms
+// retry 重试次数
+// retryDelay us
+// 当前lock超时ttl为1s，重试2次，每15ms try-lock一次
+// 请勿必只对关键业务操作路径进行加锁，缩小加锁操作范围
+// 例如库存的select-for-update，保证锁操作能在15ms内完成
+func (r *Redis) lock(ctx context.Context, key string, ttl int, retry int, retryDelay time.Duration) (locked bool, lockValue string, err error) {
 	if retry <= 0 {
 		retry = 1
 	}
@@ -55,31 +53,29 @@ func (r *Redis) lock(ctx context.Context, key string, ttl int, retry int, retryD
 	lockValue = "locked:" + xstring.RandomString(12)
 
 	for retryTimes := 0; retryTimes < retry; retryTimes++ {
-		var res interface{}
-		res, err = r.Do(ctx, "SET", key, lockValue, "PX", ttl, "NX")
+		locked, err = r.SetNxEx(ctx, key, lockValue, ttl)
 		if err != nil {
 			log.Error(ctx, "redis lock failed", log.String("key", key), log.String("error", err.Error()))
 			break
 		}
 
-		if res != nil {
-			locked = true
+		if locked {
 			break
 		}
-
 		time.Sleep(retryDelay)
 	}
 
 	return
 }
 
+// unLock unlock resource
 func (r *Redis) unLock(ctx context.Context, key string, lockValue string) (err error) {
-	res, err := redis.String(r.Do(ctx, "GET", key))
+	reply, err := r.Get(ctx, key)
 	if err != nil {
 		return
 	}
 
-	if res != lockValue {
+	if reply != lockValue {
 		err = ErrUnLockGet
 		log.Error(ctx, "unlock fail",
 			log.String("key", key),
@@ -89,26 +85,33 @@ func (r *Redis) unLock(ctx context.Context, key string, lockValue string) (err e
 		return
 	}
 
-	_, err = r.Do(ctx, "DEL", key)
-
+	_, err = r.Del(ctx, key)
 	return
 }
 
+// forceUnLock force unlock resource
 func (r *Redis) forceUnLock(ctx context.Context, key string) (err error) {
-	_, err = r.Do(ctx, "DEL", key)
-
+	_, err = r.Del(ctx, key)
 	return
-
 }
 
-func (r *Redis) Lock(ctx context.Context, key string) (err error, gotLock bool, lockValue string) {
-	return r.lock(ctx, key, _lockTime, _retry, _retryDelay)
+// Lock lock resource
+func (r *Redis) Lock(ctx context.Context, key string, expireTime ...int) (locked bool, lockValue string, err error) {
+	lockTime := _lockTime
+	if len(expireTime) > 0 {
+		lockTime = expireTime[0]
+	}
+
+	locked, lockValue, err = r.lock(ctx, key, lockTime, _retry, _retryDelay)
+	return
 }
 
+// UnLock unlock resource
 func (r *Redis) UnLock(ctx context.Context, key, lockValue string) error {
 	return r.unLock(ctx, key, lockValue)
 }
 
+// ForceUnLock force unlock resource
 func (r *Redis) ForceUnLock(ctx context.Context, key string) error {
 	return r.forceUnLock(ctx, key)
 }
